a6d9f24f907c2cf48c228b5784a56e042abc399f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2021-11-15 15:32:02 +0100

    Initial Commit

---
+++ MagicTheFiltering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTheFiltering
+//
+//  Created by Mateusz Jabłoniec on 15/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
